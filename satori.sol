4d390b634db6d3802611b9a0559d82545bb8156c
j-katzen <alto50@gmail.com>
2017-02-14 00:43:03 -0800

    comments

--- satori.sol
+++ satori.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.2;
 
+// A participant in the great Satori economy!
 contract SatoriRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
 
+// I'd have an apostrophe s...but programming
 contract satoris {
+    // other owners of the Satori economy...if he deems fit
     mapping (address => bool) private owners;
 
     public function owned() {
@@ -20,18 +23,22 @@ contract satoris {
         _;
     }
 
+    // Only Satori himself can add or remove owners
     function addOwner(address newOwner) onlySatori {
         owners[newOwner] = true;
     }
 
+    // What'd you do to get on Satori's bad side?
     function removeOwner(address shitOwner) onlySatori {
         delete owners[shitOwner];
     }
 }
 
+// A Satori...the root of what makes up the great Satori economy
 contract Satori is satoris {
-    /* Public variables of the token */
-    string public standard = 'Satori v0.2b-rc21';
+    // token veresion
+    string public version = 'Satori v0.2b-rc21';
+    // other properties and shiz
     string public name;
     string public symbol;
     uint8 public decimals;
@@ -47,7 +54,8 @@ contract Satori is satoris {
     event BanHammer(address indexed banner, address index hammered);
     event BanLifted(address indexed lifter, address index unhammered);
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
+    // The constructor...where it all began
+    // Hard coding for assurance that this coin will not be tampered with
     function Satori() {
         balanceOf[msg.sender] = 0;                          // Give the creator all initial tokens
         totalSupply = 0;                        // Update total supply
@@ -56,11 +64,13 @@ contract Satori is satoris {
         decimals = 0;                                       // Round numbers bitch
     }
 
+    // Be wary of being banned...
     modifier nonBanned {
         if (banned[msg.sender]) throw;
         _;
     }
 
+    // What can owners do?  Mint SATs of course.
     function mintSatoris(address target, uint256 mintedAmount) onlyOwners {
         balaneceOf[target] += mintedAmount;
         totalSupply += mintedAmount;
@@ -68,27 +78,30 @@ contract Satori is satoris {
         Transfer(msg.sender, target, mintedAmount);
     }
 
+    // And give the BANHAMMER
     function banHammer(address bannedAddress) onlyOwners {
         banned[bannedAddress] = true;
         BanHammer(msg.sender, bannedAddress);
     }
 
+    // Or be benevolent leaders
     function banLifted(address unbannedAddress) onlyOwners {
         delete banned[bannedAddress];
         BanLifted(msg.sender, unbannedAddress);
     }
 
-    /* Send coins */
+    // Only through trade can the Satori market flourish
     function transfer(address _to, uint256 _value) notBanned {
         if (_value < 0) throw;                               // Cannot transfer negative balance
         if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
         if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
         balanceOf[msg.sender] -= _value;                     // Subtract from the sender
         balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
+        Transfer(msg.sender, _to, _value);                   // Everyone must know of the Satori trades
     }
-    /* This unnamed function is called whenever someone tries to send ether to it */
+
+    // WTF?!
     function () {
-        throw;     // Prevents accidental sending of ether
+        throw;     // WAI?!?
     }
 }
